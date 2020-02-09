70a3b2fd2a0dd5d6617d16906c09f6bc522d7552
Luis Cabus <luiscabus@gmail.com>
2020-02-09 20:19:49 -0300

    Refactor init

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 //javac -d /projects/ufal-java-payroll/bin/ /projects/ufal-java-payroll/src/*.java
 //java Main /projects/ufal-java-payroll/bin/Main.class
 //https://www.caelum.com.br/apostila-java-orientacao-objetos/orientacao-a-objetos-basica/#exerccios-orientao-a-objetos
+//refactor init
 
 import java.util.*;
 
